8f8b6e348ebb3694b1e4b76329e63d1b328fef1d
Emir Tuncbilek <emtun@l3818-26.info.polymtl.ca>
2023-03-21 14:20:26 -0400

    tiny fix

--- sources_tp3/reer/montantAEpargnerChaqueAnneeAsm.s
+++ sources_tp3/reer/montantAEpargnerChaqueAnneeAsm.s
@@ -7,6 +7,15 @@ pushl %ebp
 movl %esp, %ebp
 # DEBUT COMPLETION
 
+
+init:
+    movl 4(%ebp), %ebx      # %ebx now accesses the "this" pointer to the class
+    xor %eax, %eax
+    xor %edx, %edx
+
+
+
+
 # FIN COMPLETION
 # NE RIEN MODIFIER APRES CETTE LIGNE
 retour:
